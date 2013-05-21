77143fec6765e9dc79ed8304d59976d16707cbe8
Jason LaPorte <ironwallaby@gmail.com>
2013-05-21 10:28:43 -0400

    Initial commit.

---
+++ index.js
@@ -0,0 +1,25 @@
+module.exports = function(haystack, needle, comparator) {
+  var low = 0,
+      high = haystack.length - 1,
+      mid, cmp;
+
+  while(low <= high) {
+    mid = (low + high) >>> 1;
+    cmp = comparator(haystack[mid], needle);
+
+    /* Too low. */
+    if(cmp < 0) 
+      low = mid + 1;
+
+    /* Too high. */
+    else if(cmp > 0)
+      high = mid - 1;
+    
+    /* Key found. */
+    else
+      return mid;
+  }
+
+  /* Key not found. */
+  return -(low + 1);
+}
---
+++ test.js
@@ -0,0 +1,19 @@
+var expect = require("chai").expect;
+
+describe("binarysearch", function() {
+  var bs  = require("./"),
+      arr = [1, 2, 2, 2, 3, 5, 9],
+      cmp = function(a, b) { return a - b; };
+
+  it("should return the index of an item in a sorted array", function() {
+    expect(bs(arr, 3, cmp)).to.equal(4);
+  });
+
+  it("should return the index of where the item would go plus one, negated, if the item is not found", function() {
+    expect(bs(arr, 4, cmp)).to.equal(-6);
+  });
+
+  it("should return any valid index if an item exists multiple times in the array", function() {
+    expect(bs(arr, 2, cmp)).to.equal(3);
+  });
+});
