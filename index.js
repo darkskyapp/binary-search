12e0279218ed3ada4edfb006548e8a3ba58c58b9
Jason LaPorte <ironwallaby@gmail.com>
2013-10-10 12:12:18 -0400

    Version 1.1 is more careful about typing rules.

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 module.exports = function(haystack, needle, comparator) {
-  var low = 0,
-      high = haystack.length - 1,
-      mid, cmp;
+  if(!Array.isArray(haystack))
+    throw new TypeError("first argument to binary search is not an array");
+
+  if(typeof comparator !== "function")
+    throw new TypeError("third argument to binary search is not a function");
+
+  var low  = 0,
+      mid  = 0,
+      high = (haystack.length - 1) >>> 0,
+      cmp  = 0;
 
   while(low <= high) {
     mid = (low + high) >>> 1;
-    cmp = comparator(haystack[mid], needle);
+    cmp = comparator(haystack[mid], needle)|0;
 
     /* Too low. */
     if(cmp < 0) 
-      low = mid + 1;
+      low  = (mid + 1) >>> 0;
 
     /* Too high. */
     else if(cmp > 0)
-      high = mid - 1;
+      high = (mid - 1) >>> 0;
     
     /* Key found. */
     else
@@ -21,5 +28,5 @@ module.exports = function(haystack, needle, comparator) {
   }
 
   /* Key not found. */
-  return -(low + 1);
+  return ~low;
 }
--- test.js
+++ test.js
@@ -5,6 +5,14 @@ describe("binarysearch", function() {
       arr = [1, 2, 2, 2, 3, 5, 9],
       cmp = function(a, b) { return a - b; };
 
+  it("should bail if not passed an array", function() {
+    expect(function() { bs(undefined, 3, cmp); }).to.throw(TypeError);
+  });
+
+  it("should bail if not passed a comparator", function() {
+    expect(function() { bs(arr, 3, undefined); }).to.throw(TypeError);
+  });
+
   it("should return the index of an item in a sorted array", function() {
     expect(bs(arr, 3, cmp)).to.equal(4);
   });
