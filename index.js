59d186ddf4caded9dce51d72e35ece722f184b28
J. T. L <ironwallaby@gmail.com>
2013-10-16 10:29:51 -0400

    Decided I was being insane with all those asm.js-like type annotations. V8 doesn't care and is smart.

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ module.exports = function(haystack, needle, comparator) {
 
   var low  = 0,
       mid  = 0,
-      high = (haystack.length - 1) >>> 0,
+      high = haystack.length - 1,
       cmp  = 0;
 
   while(low <= high) {
     /* Note that "(low + high) >>> 1" may overflow, and results in a typecast
      * to double (which gives the wrong results). */
-    mid = (low + ((high - low) >> 1)) >>> 0;
+    mid = low + (high - low >> 1);
     cmp = comparator(haystack[mid], needle)|0;
 
     /* Too low. */
     if(cmp < 0) 
-      low  = (mid + 1) >>> 0;
+      low  = mid + 1;
 
     /* Too high. */
     else if(cmp > 0)
-      high = (mid - 1) >>> 0;
+      high = mid - 1;
     
     /* Key found. */
     else
