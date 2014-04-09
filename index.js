b51b0c4dcc8c0731cb1daac674dca762b9ad27f6
Michael Marino <mmarino@gmail.com>
2014-04-09 16:34:48 +0200

    Add possibility to limit searched range.

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-module.exports = function(haystack, needle, comparator) {
+module.exports = function(haystack, needle, comparator, alow, ahigh) {
   if(!Array.isArray(haystack))
     throw new TypeError("first argument to binary search is not an array");
 
   if(typeof comparator !== "function")
     throw new TypeError("third argument to binary search is not a function");
 
-  var low  = 0,
+  var low  = (typeof alow !== 'undefined' && alow >= 0) ? alow : 0;
       mid  = 0,
-      high = haystack.length - 1,
+      high = (typeof ahigh !== 'undefined' && ahigh < haystack.length) ? ahigh : haystack.length - 1,
       cmp  = 0;
 
   while(low <= high) {
